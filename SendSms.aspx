4491d5d8ca6cb80f230dee3fb0bd8e5d483f46e0
ketul3197 <sendketulpatel@gmail.com>
2019-04-16 00:48:47 +0530

    Push-1

---
+++ SendSms.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="SendSms.aspx.cs" Inherits="SendSms" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            User Mobile
+            <asp:TextBox ID="TextBox1" runat="server" TextMode="Password"></asp:TextBox>
+            <br />
+            Password
+            <asp:TextBox ID="TextBox2" runat="server"  TextMode="Password" style="margin-bottom: 0px"></asp:TextBox>
+             </div>
+                To Mobile<asp:TextBox ID="TextBox3" runat="server" TextMode="Password"></asp:TextBox>
+              <p>
+              Msg
+            <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
+
---
+++ SendSms.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class SendSms : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string Mobile = TextBox1.Text;
+        string Pass = TextBox2.Text;
+        string ToMobile = TextBox3.Text;
+        string Msg = TextBox4.Text;
+        string Key = "";
+
+        string url = "https://smsapi.engineeringtgr.com/send/?Mobile=" + Mobile + "&Password=" + Pass + "&Key=" + Key + "&Message=" + Msg + "&To=" + ToMobile + "";
+
+        ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls | SecurityProtocolType.Ssl3;
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+        request.Method = "GET";
+        request.ContentType = "application/json";
+        request.ContentLength = 0;
+        try
+        {
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream webStream = response.GetResponseStream();
+            StreamReader streamReader = new StreamReader(webStream);
+            string response1 = streamReader.ReadToEnd();
+            Console.Out.WriteLine(response1);
+            streamReader.Close();
+            Label1.Text = "Msg Sent";
+        }catch(Exception ex)
+        {
+            Console.Out.WriteLine(ex.ToString());
+        }
+
+    }
+}
